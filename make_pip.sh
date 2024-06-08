ccf0c6a1029ecc83d55e6cab3c91b63a8fec0e7f
Марк <mark@MacBook-Air-Mark.local>
2024-06-08 10:41:20 +0800

    update

--- make_pip.sh
+++ make_pip.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-$CMD="$1"
+CMD="$1"
+PKGVER="$2"
 
 PYTHON="./venv3.11/bin/python3"
 
 $PYTHON -m build
 
 if [ "$CMD" = "upload" ] ; then
-    $PYTHON -m twine upload --repository pypi dist/*
+    if [ "$PKGVER" = "" ] ; then
+        echo "Package version not specified"
+        exit 1
+    fi
+    echo "Uploading to pypi pkg version $PKGVER"
+    $PYTHON -m twine upload --repository pypi "dist/fitxf-$PKGVER"*
 fi
 
 exit 0
