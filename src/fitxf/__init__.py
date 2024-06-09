f3e052b5613f6b6b73fad082fc3c8a1e647db8ea
Mark <mapktah@yandex.ru>
2024-06-09 17:37:34 +0800

    update

--- src/fitxf/__init__.py
+++ src/fitxf/__init__.py
@@ -4,3 +4,6 @@ from nwae.math.fit.cluster.ClusterCosineUT import ClusterCosine, ClusterCosineUn
 from nwae.math.fit.transform.FitXformUnitTest import FitXformPca, FitXformUnitTest
 from nwae.math.fit.utils.FitUtilsUnitTest import FitUtils, FitUtilsUt
 from nwae.math.fit.utils.TensorUtils import TensorUtils, TensorUtilsUnitTest
+
+# Algo
+from nwae.math.algo.encoding.Base64 import Base64
