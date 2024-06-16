8ad0bcdcdccc2270f74b48261cf11dbdc0613bd2
hohyun <mark@MacBook-Air-Mark.local>
2024-06-16 13:01:16 +0800

    update

--- src/fitxf/math/lang/encode/LangModelInterface.py
+++ src/fitxf/math/lang/encode/LangModelInterface.py
@@ -21,30 +21,6 @@ class LangModelInterface:
 성장 가능성을 시사하였다.
 """
 
-    @staticmethod
-    def get_singleton(
-            LmClass,    # class type
-            model_name = None,
-            cache_folder = None,
-            include_tokenizer = False,
-            logger = None,
-            return_key = False,
-    ):
-        # Don't include lang, because it may change to "multi"
-        key_id = str(LmClass) + '.model_name=' + str(model_name) \
-                 + '.cache_folder=' + str(cache_folder) + '.include_tokenizer=' + str(include_tokenizer)
-        sgt = Singleton(
-            class_type = LmClass,
-            logger = logger,
-        ).get_singleton(
-            key_id,
-            model_name,
-            cache_folder,
-            include_tokenizer,
-            logger,
-        )
-        return (sgt, key_id) if return_key else sgt
-
     def __init__(
             self,
             model_name = None,
