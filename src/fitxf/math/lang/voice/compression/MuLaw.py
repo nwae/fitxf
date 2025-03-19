2a479d3838a0ca971d0178ed659a6b156ce6002f
Mark <mark@Marks-MacBook-Air.local>
2025-03-19 08:53:23 +0800

    update

--- src/fitxf/math/lang/voice/compression/MuLaw.py
+++ src/fitxf/math/lang/voice/compression/MuLaw.py
@@ -11,18 +11,23 @@ class Mulaw:
         return
 
     def u_law_enc(self, x, mu = 255):
-        assert abs(x) <= 1
-        sgn = 1 if x >= 0 else -1
-        y = sgn * np.log(1 + mu * abs(x)) / np.log(1 + mu)
+        assert np.max(np.abs(x)) <= 1
+        sgn = -1 * (x < 0) + 1 * (x >= 0)
+        self.logger.debug(sgn)
+        y = sgn * np.log(1 + mu * np.abs(x)) / np.log(1 + mu)
         return y
 
     def u_law_dec(self, y, mu = 255):
-        sgn = 1 if y >= 0 else -1
-        x = sgn * ( (1 + mu) ** abs(y) - 1) / mu
+        sgn = -1 * (y < 0) + 1 * (y >= 0)
+        x = sgn * ( (1 + mu) ** np.abs(y) - 1) / mu
         return x
 
 
 if __name__ == '__main__':
-    lgr = Logging.get_default_logger(log_level=logging.INFO, propagate=False)
+    lgr = Logging.get_default_logger(log_level=logging.DEBUG, propagate=False)
     ml = Mulaw(logger=lgr)
+    x = 2 * ( (np.arange(101) / 100) - 0.5)
+    lgr.info(x)
+    x_enc = ml.u_law_enc(x=x)
+    lgr.info(x_enc)
     exit(0)
