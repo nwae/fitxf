9637adc9275ff0a6f685e9a91efd6368308f56b9
hohyun <mark@MacBook-Air-Mark.local>
2025-04-06 15:57:25 +0800

    update

--- src/fitxf/math/lang/voice/compression/MuLaw.py
+++ src/fitxf/math/lang/voice/compression/MuLaw.py
@@ -2,22 +2,38 @@ import logging
 import numpy as np
 import pandas as pd
 from fitxf.utils import Logging
+from sympy.codegen.ast import uint16
 
 
 # https://en.wikipedia.org/wiki/%CE%9C-law_algorithm
 class Mulaw:
 
+    MAX_POS = 0x1fde # 8158
+
+    # high/low, low/high, num sub intervals, interval, code
+    #    h[i] = h[i-1] + 2**(i+4)   for i >= 2
+    BIN_EDGES = [0, 1, 31, 95, 223, 479, 991, 2015, 4063]
+
+    # only need positive bins, negative bins are just bit inverse of the positive side
     BIN_INTERVALS = (
         # high/low, low/high, num sub intervals, interval, code
-        ( 8158, 4063, 16, 256, 0x80),
-        ( 4062, 2015, 16, 128, 0x90),
-        ( 2014,  991, 16,  64, 0xA0),
-        (  990,  479, 16,  32, 0xB0),
-        (  478,  223, 16,  16, 0xC0),
-        (  222,   95, 16,   8, 0xD0),
-        (   94,   31, 16,   4, 0xE0),
-        (   30,    1, 15,   2, 0xF0),
-        (    0,    0,  1,   0, 0xFF),
+        #    low[i] = low[i-1] + 2**(i+4)   for i >= 3
+        # thus
+        #    low[i] = low[3] + 2**6 + 2**7 + ... + 2**(i+5)
+        #           = low[3] + 64 (1 + 2 + .. + 2**(i-1))
+        #           = 31 + 64 (2^i - 1)
+        # Inversion to get i
+        #    i = log2( 1 + ( low[i] - 31 ) / 64 ) + 2
+        ( 8158, 4063, 16, 256, 0x80), # i=8
+        ( 4062, 2015, 16, 128, 0x90), # i=7
+        ( 2014,  991, 16,  64, 0xA0), # i=6
+        (  990,  479, 16,  32, 0xB0), # i=5
+        (  478,  223, 16,  16, 0xC0), # i=4
+        (  222,   95, 16,   8, 0xD0), # i=3
+        (   94,   31, 16,   4, 0xE0), # i=2
+        (   30,    1, 15,   2, 0xF0), # i=1
+        (    0,    0,  1,   0, 0xFF), # i=0
+        # The lower edge is just the inverted bits of the positive values on top
         (   -1,   -1,  1,   0, 0x7F),
         (  -31,   -2, 15,   2, 0x70),
         (  -95,  -32, 16,   4, 0x60),
@@ -35,11 +51,18 @@ class Mulaw:
         self.create_bins()
         return
 
+    def calculate_bin_intervals(self):
+        return [1] + (31 + 64 * (2 ** np.arange(8) - 1)).tolist()
+
+    def calculate_inverse_bin(self, value):
+        return np.log2(1 + (value - 31)/64) + 1
+
     def create_bins(self):
-        self.edge_bins = np.array([v[0] for v in self.BIN_INTERVALS])[::-1]
-        self.num_interval_bins = np.array([v[2] for v in self.BIN_INTERVALS])[::-1]
-        self.interval_bins = np.array([v[3] for v in self.BIN_INTERVALS])[::-1]
-        self.code_bins = np.array([v[4] for v in self.BIN_INTERVALS])[::-1]
+        # only need positive bins, negative bins are just bit inverse of the positive side
+        self.edge_bins = np.array([v[1] for v in self.BIN_INTERVALS if v[1]>=0])[::-1]
+        self.num_interval_bins = np.array([v[2] for v in self.BIN_INTERVALS if v[1]>=0])[::-1]
+        self.interval_bins = np.array([v[3] for v in self.BIN_INTERVALS if v[1]>=0])[::-1]
+        self.code_bins = np.array([v[4] for v in self.BIN_INTERVALS if v[1]>=0])[::-1]
         df = pd.DataFrame({
             'edge': self.edge_bins,
             'num_interval': self.num_interval_bins,
@@ -47,28 +70,41 @@ class Mulaw:
             'code': self.code_bins,
         })
         self.logger.info(df)
+        self.logger.info([hex(v) for v in self.edge_bins])
+        self.logger.info([hex(~v) for v in self.edge_bins])
+        self.logger.info([self.edge_bins[i] - self.edge_bins[i-1] for i in range(len(self.edge_bins)) if i>0])
         return
 
-    def u_law_enc_val(self, x, mu = 255):
+    def u_law_enc(self, x, mu = 255):
         assert np.max(np.abs(x)) <= 1
         sgn = -1 * (x < 0) + 1 * (x >= 0)
         y = sgn * np.log(1 + mu * np.abs(x)) / np.log(1 + mu)
+
+        y_pos = np.round(y * sgn * self.MAX_POS, decimals=0).astype(np.uint16)
+        # Use inverse formula
+        np.log2(1 + (y_pos - 30)/64) + 1
+        raise Exception(bin)
         return y
 
-    def u_law_dec_val(self, y, mu = 255):
+    def u_law_dec(self, y, mu = 255):
         assert np.max(np.abs(y)) <= 1
         sgn = -1 * (y < 0) + 1 * (y >= 0)
         x = sgn * ( (1 + mu) ** np.abs(y) - 1) / mu
+
+        m = self.edge_bins[0] * (x < 0) + self.edge_bins[-1] * (x >= 0)
+        x_d = np.round(x * m, decimals=0)
         return x
 
 
 if __name__ == '__main__':
     lgr = Logging.get_default_logger(log_level=logging.DEBUG, propagate=False)
     ml = Mulaw(logger=lgr)
+    lgr.info(ml.calculate_bin_intervals())
+    raise Exception('asdf')
     x = 2 * ( (np.arange(101) / 100) - 0.5)
     lgr.info(x)
-    x_enc = ml.u_law_enc_val(x=x)
-    x_dec = ml.u_law_dec_val(y=x_enc)
+    x_enc = ml.u_law_enc(x=x)
+    x_dec = ml.u_law_dec(y=x_enc)
     tmp = np.array([
         x.tolist(), x_enc.tolist(), x_dec.tolist()
     ]).transpose()
