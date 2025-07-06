33e966dc65edd27ef413411f91a512b2afa91a55
root <mark@MacBook-Air-Mark.local>
2025-07-06 11:35:56 +0800

    update

--- src/fitxf/math/data/Dataset.py
+++ src/fitxf/math/data/Dataset.py
@@ -13,9 +13,22 @@ class DatasetUtil:
     ):
         self.cache_dir = cache_dir
         self.logger = logger if logger is not None else logging.getLogger()
-        self.dataset = None
+        self.dataset_dict = None
         self.dataset_path, self.dataset_name = None, None
         return
+
+    def create_dataset_from_pandas(
+            self,
+            df_train: pd.DataFrame,
+            df_dev: pd.DataFrame | None = None,
+            df_test: pd.DataFrame | None = None,
+    ):
+        tmp_dict = {}
+        for k, df in [('train', df_train), ('dev', df_dev), ('test', df_test)]:
+            if df is not None:
+                tmp_dict[k] = df
+        self.dataset_dict = DatasetDict(tmp_dict)
+        return self.dataset_dict
 
     def download(
             self,
@@ -25,31 +38,31 @@ class DatasetUtil:
             dataset_name: str,
     ):
         if len(dataset_name) > 0:
-            self.dataset = load_dataset(
+            self.dataset_dict = load_dataset(
                 path = dataset_path,
                 name = dataset_name,
                 # data_dir = self.cache_dir,
             )
         else:
-            self.dataset = load_dataset(path=dataset_path)
+            self.dataset_dict = load_dataset(path=dataset_path)
         self.logger.info(
             'Dataset path "' + str(dataset_path) + '", name "' + str(dataset_name)
-            + '" downloaded, dataset type "' + str(type(self.dataset)) + '", keys ' + str(self.get_dataset_keys())
-            + ', length ' + str(len(self.dataset)) + ', sample from key "' + str(self.get_dataset_keys()[0])
-            + '": ' + str(self.dataset[self.get_dataset_keys()[0]][0:3])
+            + '" downloaded, dataset type "' + str(type(self.dataset_dict)) + '", keys ' + str(self.get_dataset_keys())
+            + ', length ' + str(len(self.dataset_dict)) + ', sample from key "' + str(self.get_dataset_keys()[0])
+            + '": ' + str(self.dataset_dict[self.get_dataset_keys()[0]][0:3])
         )
         self.dataset_path = dataset_path
         self.dataset_name = dataset_name
         return
 
     def get_dataset_keys(self):
-        return list(self.dataset.keys())
+        return list(self.dataset_dict.keys())
 
     def is_dataset_exist(
             self,
             dataset_key: str,
     ):
-        return dataset_key in self.dataset.keys()
+        return dataset_key in self.dataset_dict.keys()
 
     def get_data(
             self,
@@ -59,7 +72,7 @@ class DatasetUtil:
             # allowed values, "", "pandas
             return_type: str = "",
     ) -> Dataset | pd.DataFrame | None:
-        assert type(self.dataset) in [DatasetDict]
+        assert type(self.dataset_dict) in [DatasetDict]
 
         if not self.is_dataset_exist(dataset_key=dataset_key):
             self.logger.warning(
@@ -69,9 +82,9 @@ class DatasetUtil:
             return None
 
         if select_range > 0:
-            data = self.dataset[dataset_key].select(range(0, select_range))
+            data = self.dataset_dict[dataset_key].select(range(0, select_range))
         else:
-            data = self.dataset[dataset_key]
+            data = self.dataset_dict[dataset_key]
 
         self.logger.info(
             'Data type "' + str(type(data)) + ' for dataset key "' + str(dataset_key) + '", length ' + str(len(data))
@@ -98,9 +111,14 @@ if __name__ == '__main__':
         dataset_path = 'sentence-transformers/all-nli',
         dataset_name = 'triplet',
     )
-    ds.get_data(
+    df = ds.get_data(
         dataset_key = "train",
         select_range = 2,
         return_type = 'pandas',
     )
+    lgr.info('Data in pandas:\n' + str(df))
+
+    ds2 = DatasetUtil(logger=lgr)
+    ds2.create_dataset_from_pandas(df_train=df)
+    lgr.info('Dataset from pandas:\n' + str(ds2.dataset_dict))
     exit(0)
