d0ea0ef1cc5b7a8baa487a4360011ba0c8b855d9
root <mark@MacBook-Air-Mark.local>
2025-10-11 08:15:48 +0800

    update

--- src/fitxf/math/fit/arc/blocks/PositionalEncoding.py
+++ src/fitxf/math/fit/arc/blocks/PositionalEncoding.py
@@ -21,6 +21,8 @@ class PositionalEncoding(nn.Module):
         super(PositionalEncoding, self).__init__()
         self.logger = logger if logger is not None else logging.getLogger()
 
+        assert emb_size % 2 == 0, 'Embedding size must be even number, not ' + str(emb_size)
+
         # weights on the embedding vector, so means same length as embedding size, decrease with index
         # e.g. for emb_size = 128, it decreases from 1.0 to 0.00011548 at position-127
         # tensor([1.0000e+00, 8.6596e-01, 7.4989e-01, 6.4938e-01, 5.6234e-01, 4.8697e-01,...
@@ -40,27 +42,45 @@ class PositionalEncoding(nn.Module):
         pos_embedding[:, 1::2] = torch.cos(pos * den)
         # print(torch.cos(pos * den))
         pos_embedding = pos_embedding.unsqueeze(-2)
-        self.logger.debug(pos_embedding)
+        self.logger.debug('Position embedding: ' + str(pos_embedding))
 
         self.dropout = nn.Dropout(dropout)
         self.register_buffer('pos_embedding', pos_embedding)
         return
 
-    def forward(self, token_embedding: Tensor):
+    def forward(
+            self,
+            token_embedding: Tensor,
+    ):
         self.logger.debug(
             'Token embedding shape ' + str(token_embedding.shape)
             + ', pos embedding shape ' + str(self.pos_embedding.shape)
         )
+        self.logger.debug(
+            'Add positional embedding: ' + str(self.pos_embedding[:token_embedding.size(0), :])
+            + ' to token embedding: ' + str(token_embedding)
+        )
+        # Default transformer dimension is (sentence length, batch, ..), so we take the first index
+        # and match the sentence length with the embedding positions
         return self.dropout(token_embedding + self.pos_embedding[:token_embedding.size(0), :])
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
+    n_batch, sentence_len, embd_sz = 2, 3, 4
     pe = PositionalEncoding(
-        emb_size = 8,
+        emb_size = embd_sz,
         dropout  = 0.,
-        maxlen   = 20,
+        maxlen = sentence_len,
     )
     # 11 samples of fixed length (=20) sentences, of token embedding dim 8
-    y = pe.forward(token_embedding=torch.randint(1000, (11, 20, 8)))
-    print(y)
+    # y = pe.forward(token_embedding=torch.randint(1000, (11, 20, embd_sz)))
+    x = torch.ones(n_batch, sentence_len, embd_sz)
+    for i in range(n_batch):
+        x[i, :, :] *= (i + 1)
+    print(x)
+    # Default transformer dimension is (sentence length, batch, ..), so we need
+    # swap the first 2 dimensions
+    y = pe.forward(token_embedding=x.transpose(0, 1))
+    print(y.transpose(0, 1))
+    exit(0)
